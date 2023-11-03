b6b1398a739b99068e0b64261eca2b04624aadea
Raffaele Zippo <raffaele.zippo@ing.unipi.it>
2023-11-03 17:10:15 +0100

    Add tests for superadditive closure

--- Nancy/Nancy.Tests/MinPlusAlgebra/Curves/SuperAdditiveClosure.cs
+++ Nancy/Nancy.Tests/MinPlusAlgebra/Curves/SuperAdditiveClosure.cs
@@ -44,4 +44,100 @@ public class SuperAdditiveClosure
     //     Assert.True(Curve.Equivalent(closure, curve));
     // }
 
+    public static readonly List<(Curve curve, Curve closure)> KnownClosures = new()
+    {
+        (
+            new RateLatencyServiceCurve(1, 1),
+            new RateLatencyServiceCurve(1, 1)
+        ),
+        (
+            new Curve(new RateLatencyServiceCurve(1, 1)),
+            new Curve(new RateLatencyServiceCurve(1, 1))
+        ),
+        (
+            new Curve(
+                baseSequence: new Sequence(new List<Element>
+                {
+                    new Point(0, 0),
+                    new Segment(0, 3, 0, 0),
+                    new Point(3, 5),
+                    new Segment(3, 4, 5, 1),
+                    new Point(4, 6), new Segment(4, 5, 6, 1)
+                }), 
+                pseudoPeriodStart: 4, 
+                pseudoPeriodLength: 1, 
+                pseudoPeriodHeight: 1
+            ),
+            new Curve(
+                baseSequence: new Sequence(new List<Element>
+                {
+                    new Point(0, 0),
+                    new Segment(0, 3, 0, 0),
+                    new Point(3, 5),
+                    new Segment(3, 6, 5, 1),
+                }), 
+                pseudoPeriodStart: 3,
+                pseudoPeriodLength: 3,
+                pseudoPeriodHeight: 5
+            )
+        ),
+        (
+            new Curve(
+                baseSequence: new Sequence(new List<Element>
+                {
+                    new Point(0,0), 
+                    new Segment(0,5,0,0), 
+                    new Point(5,12), 
+                    new Segment(5,15,12,new Rational(1, 10))
+                }),
+                pseudoPeriodStart: 5,
+                pseudoPeriodLength: 10,
+                pseudoPeriodHeight: 1
+            ),
+            new Curve(
+                baseSequence: new Sequence(new List<Element>
+                {
+                    new Point(0,0), 
+                    new Segment(0,5,0,0), 
+                    new Point(5,12), 
+                    new Segment(5,10,12,new Rational(1, 10))
+                }),
+                pseudoPeriodStart: 5,
+                pseudoPeriodLength: 5,
+                pseudoPeriodHeight: 12
+            )
+        )
+    };
+
+    public static IEnumerable<object[]> KnownClosuresTestCases()
+    {
+        foreach (var (curve, closure) in KnownClosures)
+        {
+            yield return new object[] { curve, closure };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(KnownClosuresTestCases))]
+    public void KnownClosuresTest(Curve curve, Curve expected)
+    {
+        var closure = curve.SuperAdditiveClosure();
+        Assert.True(Curve.Equivalent(expected, closure));
+    }
+
+    public static IEnumerable<object[]> ClosureDominanceTestCases()
+    {
+        foreach (var (curve, _) in KnownClosures)
+        {
+            yield return new object[] { curve };
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(ClosureDominanceTestCases))]
+    public void ClosureDominanceTest(Curve curve)
+    {
+        var closure = curve.SuperAdditiveClosure();
+        Assert.True(closure >= curve);
+    }
 }
