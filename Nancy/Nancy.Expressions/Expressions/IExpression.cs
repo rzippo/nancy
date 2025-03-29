43d2cc9eddc78f55f88e75195818f620b9a2b249
Raffaele Zippo <raffele.zippo@ing.unipi.it>
2025-03-29 09:24:04 +0100

    Introduce root interface IExpression Useful for variable passing while type of the expression is not needed

---
+++ Nancy/Nancy.Expressions/Expressions/IExpression.cs
@@ -0,0 +1,68 @@
+﻿using Unipi.Nancy.Expressions.ExpressionsUtility;
+using Unipi.Nancy.Expressions.Internals;
+using Unipi.Nancy.Expressions.Visitors;
+
+namespace Unipi.Nancy.Expressions;
+
+/// <summary>
+/// Core interface for Nancy expressions.
+/// Does not specify its return or operand types, and therefore no operation.
+/// </summary>
+/// <remarks>
+/// To be used as a common root for simple variable assignments.
+/// To have computation methods, see casts to other types like <see cref="CurveExpression"/> or <see cref="RationalExpression"/>.
+/// </remarks>
+public interface IExpression
+{
+    /// <summary>
+    /// The name of the expression.
+    /// </summary>
+    public string Name { get; }
+    
+    /// <summary>
+    /// Formats the expression in LaTeX.
+    /// </summary>
+    /// <param name="depth">Level of the expression tree up to which to print the expression fully expanded.</param>
+    /// <param name="showRationalsAsName">
+    /// If true, shows rational numbers with their expression name in place of their value.
+    /// </param>
+    public string ToLatexString(
+        int depth = 20,
+        bool showRationalsAsName = false
+    );
+
+    /// <summary>
+    /// Returns the representation of the expression using characters of the Unicode character set.
+    /// </summary>
+    /// <param name="depth">Level of the expression tree up to which to print the expression fully expanded.</param>
+    /// <param name="showRationalsAsName">
+    /// If true, shows rational numbers with their expression name in place of their value.
+    /// </param>
+    public string ToUnicodeString(
+        int depth = 20,
+        bool showRationalsAsName = false
+    );
+    
+    /// <summary>
+    /// Represents the expression in textual format.
+    /// </summary>
+    public string ToString();
+    
+    /// <summary>
+    /// Evaluates the computational complexity of the expression.
+    /// </summary>
+    /// <returns>The value representative of the complexity of the expression</returns>
+    public double Estimate();
+    
+    /// <summary>
+    /// Method used for implementing the Visitor design pattern: the visited object must "accept" the visitor object.
+    /// </summary>
+    /// <param name="visitor">The Visitor object</param>
+    public void Accept(IExpressionVisitor visitor);
+    
+    /// <summary>
+    /// This method can be used as a starting point to build a symbolic path through the expression.
+    /// </summary>
+    /// <returns>Returns the position of the root of the expression</returns>
+    public ExpressionPosition RootPosition();
+}
--- Nancy/Nancy.Expressions/Expressions/IGenericExpression.cs
+++ Nancy/Nancy.Expressions/Expressions/IGenericExpression.cs
@@ -1,25 +1,23 @@
 ﻿using Unipi.Nancy.Expressions.Equivalences;
 using Unipi.Nancy.Expressions.ExpressionsUtility;
 using Unipi.Nancy.Expressions.Visitors;
+using Unipi.Nancy.MinPlusAlgebra;
+using Unipi.Nancy.Numerics;
 
 namespace Unipi.Nancy.Expressions.Internals;
 
 /// <summary>
 /// Interface which defines the rules each Nancy expression must follow.
 /// </summary>
-/// <typeparam name="T">T is the class (Curve or Rational) which
-/// represents the type the expression evaluates to</typeparam>
-public interface IGenericExpression<out T>
+/// <typeparam name="T">
+/// The type the expression evaluates to, either <see cref="Curve"/> or <see cref="Rational"/>.
+/// </typeparam>
+public interface IGenericExpression<out T> : IExpression
 {
     /// <summary>
     /// The value of the expression
     /// </summary>
     public T Value { get; }
-
-    /// <summary>
-    /// The name of the expression
-    /// </summary>
-    public string Name { get; }
 
     /// <summary>
     /// Settings for the expression (contains also the settings for the evaluation of the expression)
@@ -30,28 +28,6 @@ public interface IGenericExpression<out T>
     /// Computes the value the expression evaluates to
     /// </summary>
     public T Compute();
-
-    /// <summary>
-    /// Formats the expression in LaTeX.
-    /// </summary>
-    /// <param name="depth">Level of the expression tree up to which to print the expression fully expanded.</param>
-    /// <param name="showRationalsAsName">If true, shows rational numbers with their expression name
-    /// (<see cref="Name"/>) in place of <see cref="Value"/>.</param>
-    public string ToLatexString(
-        int depth = 20,
-        bool showRationalsAsName = false
-    );
-
-    /// <summary>
-    /// Represents the expression in textual format
-    /// </summary>
-    public string ToString();
-
-    /// <summary>
-    /// Evaluates the computational complexity of the expression.
-    /// </summary>
-    /// <returns>The value representative of the complexity of the expression</returns>
-    public double Estimate();
 
     /// <summary>
     /// Replaces every occurence of a sub-expression in the expression to which the method is applied.
@@ -84,29 +60,6 @@ public interface IGenericExpression<out T>
     /// <returns>New expression object with replaced sub-expression</returns>
     public IGenericExpression<T> ReplaceByPosition<T1>(IEnumerable<string> positionPath,
         IGenericExpression<T1> newExpression);
-
-    /// <summary>
-    /// Method used for implementing the Visitor design pattern: the visited object must "accept" the visitor object.
-    /// </summary>
-    /// <param name="visitor">The Visitor object</param>
-    public void Accept(IExpressionVisitor visitor);
-
-    /// <summary>
-    /// Returns the representation of the expression using characters of the Unicode character set.
-    /// </summary>
-    /// <param name="depth">Level of the expression tree up to which to print the expression fully expanded.</param>
-    /// <param name="showRationalsAsName">If true, shows rational numbers with their expression name
-    /// (<see cref="Name"/>) in place of <see cref="Value"/>.</param>
-    public string ToUnicodeString(
-        int depth = 20,
-        bool showRationalsAsName = false
-    );
-
-    /// <summary>
-    /// This method can be used as a starting point to build a symbolic path through the expression.
-    /// </summary>
-    /// <returns>Returns the position of the root of the expression</returns>
-    public ExpressionPosition RootPosition();
 
     /// <summary>
     /// Changes the name of the expression.
