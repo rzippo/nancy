78ac2581a0ac4953459e1c6cc1f53f021bc4aeaf
Raffaele Zippo <raffele.zippo@ing.unipi.it>
2025-04-29 15:05:41 +0200

    Nancy.Expressions.Interactive: add Latex.Show() to explicitly render expressions

---
+++ Nancy/Nancy.Expressions.Interactive/Latex.cs
@@ -0,0 +1,36 @@
+using Microsoft.DotNet.Interactive.Formatting;
+using Unipi.Nancy.Expressions.ExpressionsUtility;
+using Unipi.Nancy.Expressions.Internals;
+
+namespace Unipi.Nancy.Expressions.Interactive;
+
+public static class Latex
+{
+    /// <summary>
+    /// Displays the expression using Latex and HTML.
+    /// </summary>
+    /// <param name="expression">The expression to display.</param>
+    public static void Show<T>(
+        this IGenericExpression<T> expression
+    )
+    {
+        var html = expression.ToHtml();
+        html.DisplayAs(HtmlFormatter.MimeType);
+    }
+    
+    /// <summary>
+    /// Displays the expression in Latex syntax.
+    /// </summary>
+    /// <remarks>
+    /// Does not _render_ the Latex syntax.
+    /// Use <see cref="Show{T}"/> for that.
+    /// </remarks>
+    /// <param name="expression">The expression to display.</param>
+    public static void ShowSyntax<T>(
+        this IGenericExpression<T> expression
+    )
+    {
+        var latex = expression.ToLatexString();
+        latex.DisplayAs(PlainTextFormatter.MimeType);
+    }
+}
