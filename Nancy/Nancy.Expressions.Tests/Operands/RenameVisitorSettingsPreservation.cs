8ee7a6b05a6b84b650119ba3795083322a53def2
Raffaele Zippo <raffaele.zippo@outlook.it>
2026-08-30 00:06:03 +0200

    Nancy.Expressions.Tests: cover Settings surviving a rename

---
+++ Nancy/Nancy.Expressions.Tests/Operands/RenameVisitorSettingsPreservation.cs
@@ -0,0 +1,43 @@
+using Unipi.Nancy.Expressions.Internals;
+using Unipi.Nancy.MinPlusAlgebra;
+using Unipi.Nancy.NetworkCalculus;
+using Unipi.Nancy.Numerics;
+using Xunit;
+
+namespace Unipi.Nancy.Expressions.Tests.Operands;
+
+public class RenameVisitorSettingsPreservation
+{
+    [Fact]
+    public void WithNamePreservesSettingsOnCurveExpressions()
+    {
+        var settings = new ExpressionSettings { CacheSettings = new CacheSettings { CheapCacheSegmentThreshold = 7 } };
+        var expression = new ConcreteCurveExpression(new ConstantCurve(1), "a", settings);
+
+        var renamed = expression.WithName("b");
+
+        Assert.Same(settings, renamed.Settings);
+    }
+
+    [Fact]
+    public void WithGenerationPreservesSettingsOnCurveExpressions()
+    {
+        var settings = new ExpressionSettings { CacheSettings = new CacheSettings { CheapCacheSegmentThreshold = 7 } };
+        var expression = new ConcreteCurveExpression(new ConstantCurve(1), "a", settings);
+
+        var withGeneration = expression.WithGeneration(1);
+
+        Assert.Same(settings, withGeneration.Settings);
+    }
+
+    [Fact]
+    public void WithNamePreservesSettingsOnRationalExpressions()
+    {
+        var settings = new ExpressionSettings { CacheSettings = new CacheSettings { CheapCacheSegmentThreshold = 7 } };
+        var expression = new RationalNumberExpression(new Rational(1), "a", settings);
+
+        var renamed = expression.WithName("b");
+
+        Assert.Same(settings, renamed.Settings);
+    }
+}
